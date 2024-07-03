d8dff37539ca882f6b13b8c80a37ca3160075092
Al Samir <shahinur1591@gmail.com>
2024-07-03 20:25:05 +0600

    project setup done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "light", //
+  },
+};
